df8a4ebdd97f53296455b2495f7529625bac30e8
Extra5enS <molotkov.md@phystech.edu>
2020-08-30 10:42:28 +0300

    refuctoring

--- client.c
+++ client.c
@@ -1,10 +1,21 @@
 #include"client-server.h"
 
-
 typedef struct {
     int num;
     char * text;
 } message_t;
+
+/*
+ * Dont forget to close socket_desc!
+ */
+int server_connection_init(int* socket_desc, struct sockaddr_in* server) {
+    *socket_desc = socket(AF_INET , SOCK_STREAM , 0); 
+    server -> sin_addr.s_addr = inet_addr("0.0.0.0"); // addres of server 
+	server -> sin_family = AF_INET;
+	server -> sin_port = htons(SERVER_PORT);
+    
+    return connect(*socket_desc , (struct sockaddr*)server , sizeof(*server)); 
+}
 
 /*
  *  Working prototype without buf and nonblock recv;
@@ -13,37 +24,27 @@ void *client_thread(void* arg) {
     /* Here must be 5'000'000 messages about 8k every one
      * But here we will send one messag 5'000'000 times */
     int my_num = *(int*)arg;
-    int socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    int socket_desc;
 	struct sockaddr_in server;
-    
-    server.sin_addr.s_addr = inet_addr("0.0.0.0"); // addres of server 
-	server.sin_family = AF_INET;
-	server.sin_port = htons(SERVER_PORT);
-    
-    if(connect(socket_desc , (struct sockaddr*)&server , sizeof(server)) < 0) {
+    if(server_connection_init(&socket_desc, &server) < 0) {
         printf("Connection erron\n");
     }
+
     for(int i = 0; i < SEND_COUNT; i++) {
         char message[MESSAGE_SIZE + HEAD_SIZE]; 
         sprintf(message, "%d %d", my_num, i);        
         for(int j = strlen(message); j < HEAD_SIZE; ++j) {
             message[j] = ' ';
         }
-        int fd = open("test_message", O_RDONLY); 
-        
+
+        int fd = open("test_message", O_RDONLY);  
         read(fd, message + HEAD_SIZE, MESSAGE_SIZE);
         close(fd);
         
-        if (send(socket_desc, message, strlen(message), 0) < 0) {
-            printf("Send error\n");
-        }
+        send(socket_desc, message, strlen(message), 0);
         
         char Ans[5];
         recv(socket_desc, Ans, 5, 0);
-
-        if(i % (SEND_COUNT / 100) == 0) {
-            printf("Done %d%%\n", i / (SEND_COUNT / 100));
-        }
 
     }
     printf("Done\n");
--- server.c
+++ server.c
@@ -37,7 +37,6 @@ void task_free(task* client_task) {
     free(client_task);
 }
 
-
 typedef struct {
     int next_message_num;
     int fd;
@@ -46,31 +45,33 @@ typedef struct {
 // when you finish to use file_info? you must write close(file_info.fd); to close file!
 file_info file_info_init(char* filename) {
     file_info fileinfo;
-    fileinfo.fd = open(filename, O_APPEND|O_CREAT|O_WRONLY, S_IWRITE|S_IREAD);
+    fileinfo.fd = open(filename, O_APPEND|O_CREAT|O_WRONLY, __S_IWRITE|__S_IREAD);
     fileinfo.next_message_num = 0;
     return fileinfo;
 } 
 
+
+void server_init(int* socket_desc, struct sockaddr_in* server) {
+    *socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    server -> sin_family = AF_INET;
+	server -> sin_addr.s_addr = INADDR_ANY;
+	server -> sin_port = htons( SERVER_PORT );
+    
+    bind(*socket_desc,(struct sockaddr *)server , sizeof(*server));
+    listen(*socket_desc, THREAD_COUNT);
+}
+
 task* global_task;
-file_info fd_for_client_threads[THREAD_COUNT];
+file_info files_info[THREAD_COUNT];
 
 // Done
 void* receiver() {
-    int socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    int addrlen = sizeof(struct sockaddr_in);
+    int socket_desc;
+    //int addrlen = sizeof(struct sockaddr_in);
 	int client_sockets[THREAD_COUNT];
     struct sockaddr_in server;
-    struct sockaddr_in clients[9];
-    
-    server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( SERVER_PORT );
-   
-    bind(socket_desc,(struct sockaddr *)&server , sizeof(server));
-    listen(socket_desc, THREAD_COUNT);
-    
-    // start to revc new message from client;
-    
+    //struct sockaddr_in clients[9];
+    server_init(&socket_desc, &server); 
     for(int i = 0; i < THREAD_COUNT; ++i) {
         client_sockets[i] = accept(socket_desc,NULL /*(struct sockaddr *)&clients[i]*/,0 /*(socklen_t*)&addrlen*/);
     }
@@ -78,13 +79,12 @@ void* receiver() {
     char* client_message = calloc(MESSAGE_SIZE + HEAD_SIZE, 1); 
     for(;;) {
         for(int i = 0; i < THREAD_COUNT; ++i) {
-            if(recv(client_sockets[i], client_message, MESSAGE_SIZE + HEAD_SIZE, 0 /*MSG_DONTWAIT*/) > 0) {
+            if(recv(client_sockets[i], client_message, MESSAGE_SIZE + HEAD_SIZE, MSG_DONTWAIT) > 0) {
                 task* client_task = task_init(client_sockets[i], client_message);
                 task* null_ptr = NULL;
                 
                 while(atomic_load(&global_task) || !atomic_cas(&global_task, &null_ptr, client_task)) {
                     usleep(10);
-                    //printf("%p\n", global_task);
                 }
 
                 client_message = calloc(MESSAGE_SIZE + HEAD_SIZE, 1);       
@@ -105,18 +105,18 @@ void* worker() {
             usleep(10);
             my_task = atomic_load(&global_task);
         } while(!my_task || !atomic_cas(&global_task, &my_task, NULL));
-        
+
         printf("%d %d\n", my_task->thread_num, my_task -> message_num); 
-        while(my_task -> message_num != atomic_load(&fd_for_client_threads[my_task -> thread_num].next_message_num)) {
+        while(my_task -> message_num != atomic_load(&files_info[my_task -> thread_num].next_message_num)) {
             usleep(10);
         }
-        write(fd_for_client_threads[my_task -> thread_num].fd, my_task -> client_message + HEAD_SIZE, strlen(my_task -> client_message + HEAD_SIZE));
-        fsync(fd_for_client_threads[my_task -> thread_num].fd);
-        
+
+        write(files_info[my_task -> thread_num].fd, my_task -> client_message + HEAD_SIZE, strlen(my_task -> client_message + HEAD_SIZE));
+        fsync(files_info[my_task -> thread_num].fd);
         send(my_task -> client_socket, "Done", 5, 0);
     
-        int next_message_num = fd_for_client_threads[my_task -> thread_num].next_message_num + 1;
-        atomic_store(&fd_for_client_threads[my_task -> thread_num].next_message_num, next_message_num);
+        int next_message_num = files_info[my_task -> thread_num].next_message_num + 1;
+        atomic_store(&files_info[my_task -> thread_num].next_message_num, next_message_num);
         task_free(my_task);
     }
     pthread_exit(0);
@@ -131,7 +131,7 @@ int main() {
     for(int i = 0; i < THREAD_COUNT; ++i) {
         char filename[3];
         sprintf(filename, "%d", i);
-        fd_for_client_threads[i] = file_info_init(filename);
+        files_info[i] = file_info_init(filename);
     } 
 
     pthread_create(&receiver_thread, NULL, receiver, NULL);
