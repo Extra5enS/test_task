4d1dd81845e834566697d90645aa6ab1d9740671
Extra5enS <molotkov.md@phystech.edu>
2020-09-03 13:43:50 +0300

    creat all for task, need to finish client_thread

--- client-server.h
+++ client-server.h
@@ -23,6 +23,7 @@
 #define atomic_store(ptr, new_value)    __atomic_store_n(ptr, new_value, __ATOMIC_SEQ_CST)
 #define atomic_cas(ptr, old_value_ptr, new_value) __atomic_compare_exchange_n(ptr, old_value_ptr, new_value, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)
 #define atomic_increment(ptr) __atomic_fetch_add(ptr, 1, __ATOMIC_SEQ_CST)
+#define atomic_decrement(ptr) __atomic_fetch_add(ptr, -1, __ATOMIC_SEQ_CST)
 #define skip_head(prt) (prt + HEAD_SIZE)
 
 
--- client.c
+++ client.c
@@ -6,6 +6,38 @@
 #define FILE_NAME "test_message"
 #define MAX_BUF_SIZE 1
 
+typedef struct {
+    int need_to_delete;
+    int sd;
+    int is_close;
+} delete_node;
+
+typedef struct {
+    delete_node* array;
+    int size;
+    int sd_count;
+} dn_info;
+
+void dn_info_init(dn_info* dn, int size) {
+    dn -> array = calloc(size, sizeof(delete_node));
+    dn -> size = size;
+    dn -> sd_count = 0;
+    for(int i = 0; i < size; i++) {
+        dn -> array[i].need_to_delete = 0;
+        dn -> array[i].sd = 0;
+        dn -> array[i].is_close = 0;
+    }
+}
+
+delete_node* dn_info_at(dn_info* dn, int num) {
+    return dn -> array + num;
+}   
+
+void dn_info_free(dn_info* dn) {
+    free(dn -> array);
+}
+
+
 /*
  * Dont forget to close socket_desc!
  */
@@ -25,6 +57,8 @@ void creat_message(int fd, char* space_for_message, int thread_num, int message_
     read(fd, skip_head(space_for_message), MESSAGE_SIZE); 
 }
 
+dn_info dninfo; 
+
 /*
  *  Working prototype without buf and nonblock recv;
  */
@@ -40,6 +74,8 @@ void *client_thread(void* arg) {
         perror("Connection erron\n");
         exit(-1);
     }
+    dninfo.array[my_num].sd = socket_desc;
+    atomic_increment(&dninfo.sd_count);
 
     for(int i = 0; i < SEND_COUNT; i++) {
         char* message = calloc(MESSAGE_SIZE + HEAD_SIZE, 1); 
@@ -56,7 +92,9 @@ void *client_thread(void* arg) {
         int num;
         do {
             num = -1;
-            recv(socket_desc, &num, sizeof(int), 0);
+           /*
+            * logic to work with income segment
+            */
             if(num != -1) {
                 string_array_delete(&sarray);
             }
@@ -76,10 +114,21 @@ void *client_thread(void* arg) {
 int main() {
     pthread_t thread_mass[THREAD_COUNT];
     int num_mass[THREAD_COUNT];
+    dn_info_init(&dninfo, THREAD_COUNT);
     for(int i = 0; i < THREAD_COUNT; ++i) {
         num_mass[i] = i;
         pthread_create(&thread_mass[i], NULL, client_thread, &num_mass[i]);
     }
+    while(dninfo.sd_count != THREAD_COUNT);
+
+    while(dninfo.sd_count != 0) {
+        for(int i = 0; i < THREAD_COUNT; ++i) {
+            int num;
+            recv(dninfo.array[i].sd, &num, sizeof(int), 0);
+            atomic_increment(&dninfo.array[i].need_to_delete);
+        }
+    }
+
     for(int i = 0; i < THREAD_COUNT; ++i) {
         pthread_join(thread_mass[i], NULL);
     }
